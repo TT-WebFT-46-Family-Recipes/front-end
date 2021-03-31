581ac3c0b4ab789a97468d238f5406ddfd74fd9a
Randy Ortiz <Randyortiz28@gmail.com>
2021-03-31 13:49:53 -0400

    Loader is complete

--- family-recipes/src/store/reducers/index.js
+++ family-recipes/src/store/reducers/index.js
@@ -1,97 +1,88 @@
-import { 
-    FETCH_RECIPES_START,
-    FETCH_RECIPES_SUCCESS,
-    FETCH_RECIPES_ERROR,
-    ADD_RECIPE,
-    ADD_RECIPE_FAIL,
-    DELETE_RECIPE,
-    DELETE_RECIPE_FAIL,
-    EDIT_RECIPE,
-    EDIT_RECIPE_FAIL 
+import {
+  FETCH_RECIPES_START,
+  FETCH_RECIPES_SUCCESS,
+  FETCH_RECIPES_ERROR,
+  ADD_RECIPE,
+  ADD_RECIPE_FAIL,
+  DELETE_RECIPE,
+  DELETE_RECIPE_FAIL,
+  EDIT_RECIPE,
+  EDIT_RECIPE_FAIL,
 } from '../actions'
 
 const InitialState = {
-    users: {},
-    recipes: {},
-    isLoading: true,
-    error: ''
+  users: {},
+  recipes: {},
+  isLoading: true,
+  error: '',
 }
 
 export const reducer = (state = InitialState, action) => {
-    switch(action.type) {
-        case FETCH_RECIPES_START:
-            return {
-                ...state,
-                isLoading: true,
-                recipes: [],
-                error: ''
-            }
-        case FETCH_RECIPES_SUCCESS:
-            return {
-                ...state,
-                isLoading: false,
-                recipes: action.payload,
-                error: ''
-            }
-        case FETCH_RECIPES_ERROR:
-            return {
-                ...state,
-                isLoading: false,
-                recipes: [],
-                error: action.payload
-            }
-        case ADD_RECIPE:
-            return {
-                ...state,
-                isLoading: false,
-                recipes: [
-                    state.recipes,
-                    action.payload
-                ],
-                error: ''
-            }
-        case ADD_RECIPE_FAIL:
-            return {
-                ...state,
-                isLoading: false,
-                recipes: [],
-                error: action.payload
-            }
-        case DELETE_RECIPE:
-            return {
-                ...state,
-                isLoading: false,
-                recipes: [
-                    state.recipes,
-                    action.payload
-                ],
-                error: action.payload
-            }
-        case DELETE_RECIPE_FAIL:
-            return {
-                ...state,
-                isLoading: false,
-                recipes: [],
-                error: action.payload
-            }
-        case EDIT_RECIPE:
-            return {
-                ...state,
-                isLoading: false,
-                recipes: [
-                    state.recipes,
-                    action.payload
-                ],
-                error: action.payload
-            }
-        case EDIT_RECIPE_FAIL:
-            return {
-                ...state,
-                isLoading: false,
-                recipes: [],
-                error: action.payload
-            }
-        default:
-            return state
-    }
+  switch (action.type) {
+    case FETCH_RECIPES_START:
+      return {
+        ...state,
+        isLoading: true,
+        recipes: [],
+        error: '',
+      }
+    case FETCH_RECIPES_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        recipes: action.payload,
+        error: '',
+      }
+    case FETCH_RECIPES_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        recipes: [],
+        error: action.payload,
+      }
+    case ADD_RECIPE:
+      return {
+        ...state,
+        isLoading: false,
+        recipes: [state.recipes, action.payload],
+        error: '',
+      }
+    case ADD_RECIPE_FAIL:
+      return {
+        ...state,
+        isLoading: false,
+        recipes: [],
+        error: action.payload,
+      }
+    case DELETE_RECIPE:
+      return {
+        ...state,
+        isLoading: false,
+        recipes: [state.recipes, action.payload],
+        error: action.payload,
+      }
+    case DELETE_RECIPE_FAIL:
+      return {
+        ...state,
+        isLoading: false,
+        recipes: [],
+        error: action.payload,
+      }
+    case EDIT_RECIPE:
+      return {
+        ...state,
+        isLoading: false,
+        recipes: [state.recipes, action.payload],
+        error: action.payload,
+      }
+    case EDIT_RECIPE_FAIL:
+      return {
+        ...state,
+        isLoading: false,
+        recipes: [],
+        error: action.payload,
+      }
+    default:
+      return state
+  }
 }
