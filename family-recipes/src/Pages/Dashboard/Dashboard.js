24836a0df7e707cd03d252d5ca789b203def6aa1
g3or3 <>
2021-03-30 23:57:10 -0400

    updates to the dashboard component

--- family-recipes/src/Pages/Dashboard/Dashboard.js
+++ family-recipes/src/Pages/Dashboard/Dashboard.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Header";
 import styled from "styled-components";
 import { StyledFilters, StyledRecipeContainer } from "./styles";
+import img from "../../Assets/logo.png";
 
 const StyledDashboard = styled.section`
-	height: 88vh;
+	height: 100vh;
 	width: 100%;
-	display: flex;
+	position: fixed;
 `;
 
 const Dashboard = ({ signedIn, signOut }) => {
+	const [clicked, setClicked] = useState(false);
+
+	const click = () => {
+		setClicked((clicked) => !clicked);
+	};
+
 	return (
 		<>
 			<Header signedIn={signedIn} signOut={signOut} />
@@ -20,8 +27,221 @@ const Dashboard = ({ signedIn, signOut }) => {
 					</div>
 				</StyledFilters>
 				<StyledRecipeContainer>
-					<div className="bg-img"></div>
-					<div className="recipe-modal"></div>
+					<div className="bg-img">
+						<div className="recipe">
+							<h3
+								align="center"
+								style={{
+									borderBottom:
+										"solid 2px rgba(0, 0, 0, 0.1)",
+									padding: "5% 0",
+									margin: 0,
+								}}
+							>
+								Title
+							</h3>
+						</div>
+						<div className="recipe">
+							<h3
+								align="center"
+								style={{
+									borderBottom:
+										"solid 2px rgba(0, 0, 0, 0.1)",
+									padding: "5% 0",
+									margin: 0,
+								}}
+							>
+								Title
+							</h3>
+						</div>
+						<div className="recipe">
+							<h3
+								align="center"
+								style={{
+									borderBottom:
+										"solid 2px rgba(0, 0, 0, 0.1)",
+									padding: "5% 0",
+									margin: 0,
+								}}
+							>
+								Title
+							</h3>
+						</div>
+						<div className="recipe">
+							<h3
+								align="center"
+								style={{
+									borderBottom:
+										"solid 2px rgba(0, 0, 0, 0.1)",
+									padding: "5% 0",
+									margin: 0,
+								}}
+							>
+								Title
+							</h3>
+						</div>
+						<div className="recipe">
+							<h3
+								align="center"
+								style={{
+									borderBottom:
+										"solid 2px rgba(0, 0, 0, 0.1)",
+									padding: "5% 0",
+									margin: 0,
+								}}
+							>
+								Title
+							</h3>
+						</div>
+						<div className="recipe">
+							<h3
+								align="center"
+								style={{
+									borderBottom:
+										"solid 2px rgba(0, 0, 0, 0.1)",
+									padding: "5% 0",
+									margin: 0,
+								}}
+							>
+								Title
+							</h3>
+						</div>
+						<div className="recipe">
+							<h3
+								align="center"
+								style={{
+									borderBottom:
+										"solid 2px rgba(0, 0, 0, 0.1)",
+									padding: "5% 0",
+									margin: 0,
+								}}
+							>
+								Title
+							</h3>
+						</div>
+						<div className="recipe">
+							<h3
+								align="center"
+								style={{
+									borderBottom:
+										"solid 2px rgba(0, 0, 0, 0.1)",
+									padding: "5% 0",
+									margin: 0,
+								}}
+							>
+								Title
+							</h3>
+						</div>
+						<div className="recipe">
+							<h3
+								align="center"
+								style={{
+									borderBottom:
+										"solid 2px rgba(0, 0, 0, 0.1)",
+									padding: "5% 0",
+									margin: 0,
+								}}
+							>
+								Title
+							</h3>
+						</div>
+						<div className="recipe">
+							<h3
+								align="center"
+								style={{
+									borderBottom:
+										"solid 2px rgba(0, 0, 0, 0.1)",
+									padding: "5% 0",
+									margin: 0,
+								}}
+							>
+								Title
+							</h3>
+						</div>
+						<div className="recipe">
+							<h3
+								align="center"
+								style={{
+									borderBottom:
+										"solid 2px rgba(0, 0, 0, 0.1)",
+									padding: "5% 0",
+									margin: 0,
+								}}
+							>
+								Title
+							</h3>
+						</div>
+						<div className="recipe">
+							<h3
+								align="center"
+								style={{
+									borderBottom:
+										"solid 2px rgba(0, 0, 0, 0.1)",
+									padding: "5% 0",
+									margin: 0,
+								}}
+							>
+								Title
+							</h3>
+						</div>
+						<div className="recipe">
+							<h3
+								align="center"
+								style={{
+									borderBottom:
+										"solid 2px rgba(0, 0, 0, 0.1)",
+									padding: "5% 0",
+									margin: 0,
+								}}
+							>
+								Title
+							</h3>
+						</div>
+						<div className="recipe">
+							<h3
+								align="center"
+								style={{
+									borderBottom:
+										"solid 2px rgba(0, 0, 0, 0.1)",
+									padding: "5% 0",
+									margin: 0,
+								}}
+							>
+								Title
+							</h3>
+						</div>
+						<div className="recipe">
+							<h3
+								align="center"
+								style={{
+									borderBottom:
+										"solid 2px rgba(0, 0, 0, 0.1)",
+									padding: "5% 0",
+									margin: 0,
+								}}
+							>
+								Title
+							</h3>
+						</div>
+						<div className="recipe">
+							<h3
+								align="center"
+								style={{
+									borderBottom:
+										"solid 2px rgba(0, 0, 0, 0.1)",
+									padding: "5% 0",
+									margin: 0,
+								}}
+							>
+								Title
+							</h3>
+						</div>
+					</div>
+					<div
+						className={
+							clicked ? "recipe-modal" : "recipe-modal hidden"
+						}
+					></div>
 				</StyledRecipeContainer>
 			</StyledDashboard>
 		</>
--- family-recipes/src/Pages/Dashboard/styles.js
+++ family-recipes/src/Pages/Dashboard/styles.js
@@ -17,9 +17,14 @@ const StyledFilters = styled.div`
 		align-items: center;
 		height: 80%;
 		width: 90%;
-		margin: 0 0 35%;
+		margin: 3% 0 35%;
 		background: white;
 		border-radius: 20px;
+		overflow: scroll;
+	}
+
+	.filters::-webkit-scrollbar {
+		display: none;
 	}
 
 	input {
@@ -48,17 +53,25 @@ const StyledFilters = styled.div`
 
 const StyledRecipeContainer = styled.div`
 	width: 80%;
+	height: 100%;
 	position: fixed;
 	left: 20%;
 	border-left: solid 2px rgba(0, 0, 0, 0.1);
 
 	.bg-img {
-		position: relative;
-		height: 100vh;
+		height: 89vh;
 		width: 100%;
 		display: flex;
+		flex-wrap: wrap;
 		align-items: center;
-		justify-content: center;
+		justify-content: flex-start;
+		overflow: scroll;
+		scroll-behavior: smooth;
+		background-attachment: fixed;
+	}
+
+	.bg-img::-webkit-scrollbar {
+		display: none;
 	}
 
 	.bg-img::before {
@@ -71,6 +84,40 @@ const StyledRecipeContainer = styled.div`
 		right: 0px;
 		bottom: 0px;
 		left: 0px;
+	}
+
+	.recipe-modal {
+		z-index: 1;
+		width: 40%;
+		height: 60%;
+		background-color: #ffffff;
+		position: absolute;
+		top: 14%;
+		right: 0;
+		bottom: 0;
+		left: 30%;
+		border: solid 2px rgba(0, 0, 0, 0.1);
+		border-radius: 10%;
+		overflow: scroll;
+	}
+
+	.hidden {
+		display: none;
+	}
+
+	.recipe-modal::-webkit-scrollbar {
+		display: none;
+	}
+
+	.recipe {
+		z-index: 1;
+		width: 25%;
+		height: 40%;
+		margin: 3% 3.9%;
+		background-color: white;
+		border-radius: 10%;
+		border: solid 2px rgba(0, 0, 0, 0.1);
+		opacity: 0.95;
 	}
 `;
 
