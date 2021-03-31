9511306d82f1dce7f245a8b5548b706ebb3941aa
g3or3 <>
2021-03-31 15:28:49 -0400

    fixed css selector conflicts

--- family-recipes/src/App.js
+++ family-recipes/src/App.js
@@ -1,28 +1,28 @@
-import { BrowserRouter as Router } from 'react-router-dom'
-import { Switch, Route } from 'react-router-dom'
-import LoginPage from './Pages/Login_Register/LoginPage'
-import Dashboard from './Pages/Dashboard/Dashboard'
-import { useState } from 'react'
+import { BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import LoginPage from "./Pages/Login_Register/LoginPage";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import { useState } from "react";
 
-import React from 'react'
-import RecipeEntry from './recipeForm/RecipeEntry'
+import React from "react";
+import RecipeEntry from "./recipeForm/RecipeEntry";
 
 function App() {
-  const [signedIn, setSignedIn] = useState(false)
+	const [signedIn, setSignedIn] = useState(false);
 
-  return (
-    // <Router>
-    //   <Switch>
-    //     <Route path="/dashboard">
-    //       <Dashboard signedIn={signedIn} signOut={setSignedIn} />
-    //     </Route>
-    //     <Route path="/">
-    //       <LoginPage signedIn={signedIn} signOut={setSignedIn} />
-    //     </Route>
-    //   </Switch>
-    // </Router>
-    <RecipeEntry/>
-  )
+	return (
+		// <Router>
+		// 	<Switch>
+		// 		<Route path="/dashboard">
+		// 			<Dashboard signedIn={signedIn} signOut={setSignedIn} />
+		// 		</Route>
+		// 		<Route path="/">
+		// 			<LoginPage signedIn={signedIn} signOut={setSignedIn} />
+		// 		</Route>
+		// 	</Switch>
+		// </Router>
+		<RecipeEntry />
+	);
 }
 
-export default App
+export default App;
