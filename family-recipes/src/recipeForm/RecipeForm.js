c0e77111ca130eabedb02ea539106037ef8343ff
unknown <you@example.com>
2021-03-31 19:54:24 -0500

    adjusted container styling

--- family-recipes/src/recipeForm/RecipeForm.js
+++ family-recipes/src/recipeForm/RecipeForm.js
@@ -1,131 +1,128 @@
-import React from "react";
-import "./RecipeForm.css";
+import React from 'react';
+import './RecipeForm.css'
 
 export default function RecipeForm(props) {
-	const { values, submit, change, disabled, errors } = props;
+    const { values, submit, change, disabled, errors } = props;
+  
+    const onSubmit = (evt) => {
+      evt.preventDefault();
+      submit();
+    };
+  
+    const onChange = (evt) => {
+      /* 🔥 FIX THIS SO IT ALSO WORKS WITH CHECKBOXES */
+      const { name, value, type, checked } = evt.target;
+      const valueToUse = type === "checkbox" ? checked : value;
+      change(name, valueToUse);
+    };
+  
+    return (
+      
+      <form className="formContainer" onSubmit={onSubmit}>
+        <div className = "nestedContainer">
+        
+        <div className="form-group-submit">
+        
+        <fieldset className ="recipeContainer">
+          <legend>Add a Recipe</legend>
 
-	const onSubmit = (evt) => {
-		evt.preventDefault();
-		submit();
-	};
+          {/*////////// TEXT INPUTS //////////*/}
 
-	const onChange = (evt) => {
-		/* 🔥 FIX THIS SO IT ALSO WORKS WITH CHECKBOXES */
-		const { name, value, type, checked } = evt.target;
-		const valueToUse = type === "checkbox" ? checked : value;
-		change(name, valueToUse);
-	};
+          <div className= "topInputs">
+            <div className ="title">
+          <label>
+            <p className="pRecipe">Title&nbsp;</p>
+            <input
+              value={values.title}
+              onChange={onChange}
+              name="title"
+              type="text"
+              placeholder = "name of recipe"
+            />
+          </label>
+          </div>
+          <div className = "source">
+          <label>
+            <p className="pRecipe">Source&nbsp;</p>
+            <input
+              value={values.source}
+              onChange={onChange}
+              name="source"
+              type="text"
+              placeholder = "source of recipe"
+            />
+          </label>
+          </div>
+          </div>
+          <div className= "ing_ins">
 
-	return (
-		<form className="formContainer" onSubmit={onSubmit}>
-			<div className="nestedContainer">
-				<div className="form-group-submit">
-					<fieldset className="recipeContainer">
-						<legend>Add a Recipe</legend>
+              {/*///////// DROPDOWN ////////// */}
+          <div className="category">
+          <label>
+            category&nbsp;
+            <select onChange={onChange} value={values.category} name="category">
+              <option value="">- Select an option -</option>
+              <option value="dinner">dinner</option>
+              <option value="chicken">chicken</option>
+              <option value="dessert">dessert</option>
+              <option value="pasta">pasta</option>
+              <option value="other">other</option>
+            </select>
+          </label>
+          </div>
 
-						{/*////////// TEXT INPUTS //////////*/}
+          <div>
+          <fieldset className = 'ingredients'>
+          <legend>Ingredients</legend>
+          <label>
+            <textarea
+              value={values.ingredients}
+              onChange={onChange}
+              name="ingredients"
+              type="text"
+              size = "60"
+              rows="5" cols="60"
+            />
+          </label>
+          </fieldset>
+          </div>
+          <div>
+          <fieldset className="instructions">
+          <legend>Instructions</legend>
+            <label >
+            
+              <textarea
+                value={values.instructions}
+                onChange={onChange}
+                name="instructions"
+                type="text"
+                size = "60"
+                rows="5" cols="60"
+              />
+            </label>
+            </fieldset>
+          </div>
+        </div>
+ 
+        
+         
 
-						<div className="topInputs">
-							<div className="title">
-								<label>
-									<p className="pRecipe">Title&nbsp;</p>
-									<input
-										className="formInput"
-										value={values.title}
-										onChange={onChange}
-										name="title"
-										type="text"
-										placeholder="name of recipe"
-									/>
-								</label>
-							</div>
-							<div className="source">
-								<label>
-									<p className="pRecipe">Source&nbsp;</p>
-									<input
-										className="formInput"
-										value={values.source}
-										onChange={onChange}
-										name="source"
-										type="text"
-										placeholder="source of recipe"
-									/>
-								</label>
-							</div>
-						</div>
-						<div className="ing_ins">
-							{/*///////// DROPDOWN ////////// */}
-							<div className="category">
-								<label>
-									category&nbsp;
-									<select
-										onChange={onChange}
-										value={values.category}
-										name="category"
-									>
-										<option value="">
-											- Select an option -
-										</option>
-										<option value="dinner">dinner</option>
-										<option value="chicken">chicken</option>
-										<option value="dessert">dessert</option>
-										<option value="pasta">pasta</option>
-										<option value="other">other</option>
-									</select>
-								</label>
-							</div>
+           {/* 🔥 DISABLE THE BUTTON */}
 
-							<div>
-								<fieldset className="ingredients">
-									<legend>Ingredients</legend>
-									<label>
-										<textarea
-											value={values.ingredients}
-											onChange={onChange}
-											name="ingredients"
-											type="text"
-											size="60"
-											rows="5"
-											cols="60"
-										/>
-									</label>
-								</fieldset>
-							</div>
-							<div>
-								<fieldset className="instructions">
-									<legend>Instructions</legend>
-									<label>
-										<textarea
-											value={values.instructions}
-											onChange={onChange}
-											name="instructions"
-											type="text"
-											size="60"
-											rows="5"
-											cols="60"
-										/>
-									</label>
-								</fieldset>
-							</div>
-						</div>
+           <button className = "recipeButton" disabled={disabled}>Submit Recipe</button>
 
-						{/* 🔥 DISABLE THE BUTTON */}
-
-						<button className="recipeButton" disabled={disabled}>
-							Submit Recipe
-						</button>
-
-						<div className="errors">
-							{/* 🔥 RENDER THE VALIDATION ERRORS HERE */}
-							<div>{errors.title}</div>
-							<div>{errors.source}</div>
-							<div>{errors.ingredients}</div>
-							<div>{errors.instructions}</div>
-							<div>{errors.category}</div>
-						</div>
-					</fieldset>
-				</div>
-			</div>
-		</form>
-	);
-}
+           <div className="errors">
+            {/* 🔥 RENDER THE VALIDATION ERRORS HERE */}
+            <div>{errors.title}</div>
+            <div>{errors.source}</div>
+            <div>{errors.ingredients}</div>
+            <div>{errors.instructions}</div>
+            <div>{errors.category}</div>
+          </div>
+          
+          </fieldset>
+        </div>
+        </div>
+      </form>
+    );
+  }
