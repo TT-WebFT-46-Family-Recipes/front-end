6cc341d80feaa47185f9e041397ddbbad0be16b7
Randy Ortiz <Randyortiz28@gmail.com>
2021-03-31 21:27:20 -0400

    added changes

--- family-recipes/src/App.js
+++ family-recipes/src/App.js
@@ -1,27 +1,27 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import { Switch, Route } from "react-router-dom";
-import LoginPage from "./Pages/Login_Register/LoginPage";
-import Dashboard from "./Pages/Dashboard/Dashboard";
-import { useState } from "react";
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
+import LoginPage from './Pages/Login_Register/LoginPage'
+import Dashboard from './Pages/Dashboard/Dashboard'
+import { useState } from 'react'
 
-import React from "react";
-import RecipeEntry from "./recipeForm/RecipeEntry";
+import React from 'react'
+import RecipeEntry from './recipeForm/RecipeEntry'
 
 function App() {
-	const [signedIn, setSignedIn] = useState(false);
+  const [signedIn, setSignedIn] = useState(false)
 
-	return (
-		<Router>
-			<Switch>
-				<Route path="/dashboard">
-					<Dashboard signedIn={signedIn} signIn={setSignedIn} />
-				</Route>
-				<Route path="/">
-					<LoginPage signedIn={signedIn} signIn={setSignedIn} />
-				</Route>
-			</Switch>
-		</Router>
-	);
+  return (
+    <Router>
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard signedIn={signedIn} signIn={setSignedIn} />
+        </Route>
+        <Route path="/">
+          <LoginPage signedIn={signedIn} signIn={setSignedIn} />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
